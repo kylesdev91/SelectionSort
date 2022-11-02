98d35a5d3bd31a47b139af420506dc8238668141
Kyle Hernandez <kyle.hernandez@student.allenisd.org>
2022-11-01 20:01:04 -0500

    added prepration data

--- main.swift
+++ main.swift
@@ -1,14 +1,9 @@
-
-
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedIntegers = [5, 1, 4, 2, 8]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
-
-
+var newArray: [String] = []
+while let nextLine = readLine() {
+    newArray.append(nextLine)
+}
 // Add your code below:
-func swap(integers: inout [Int], firstIndex: Int, secondIndex: Int) {
+func swap(integers: inout [String], firstIndex: Int, secondIndex: Int) {
     let temp = integers[firstIndex]
     integers[firstIndex] = integers[secondIndex]
     integers[secondIndex] = temp
@@ -17,8 +12,8 @@ func swap(integers: inout [Int], firstIndex: Int, secondIndex: Int) {
 var passCount = 0
 var swapCount = 0
 var swaps = 0
-var sortedIntegers = unsortedIntegers
-var temp = 0
+var sortedIntegers = newArray
+var temp = ""
 let i = 0
 var j = 0
 print("Pass: \(passCount), Swaps: \(swaps)/\(swapCount), Array: \(sortedIntegers)")
@@ -27,7 +22,7 @@ for i in 0 ..< sortedIntegers.count - 1 {
     swapCount += 1
     var less = i
     for j in i + 1 ..< sortedIntegers.count {
-        if sortedIntegers[j] < sortedIntegers[less] {
+        if sortedIntegers[j].lowercased() < sortedIntegers[less].lowercased() {
             less = j
         }
     }
